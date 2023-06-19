024818f91eb295763e0beb3e0d10204774ee5f41
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2023-06-19 15:14:13 +0100

    WIP

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -38,6 +38,24 @@
         {{-- Icons --}}
         <script src="{{ Config::get('theme.icons') }}" crossorigin="anonymous"></script>
 
+        {{-- Analytics --}}
+        @if (Config::get('services.google.analytics.key'))
+            @production
+
+                <!-- Google Tag Manager -->
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','{{ Config::get('services.google.analytics.key') }}');</script>
+                <!-- End Google Tag Manager -->
+
+            @endproduction
+        @endif
+
+        {{-- Slot --}}
+        @includeIf('views.head')
+
         {{-- Vite --}}
         @vite(['bootstrap/app.js'])
 
@@ -45,6 +63,18 @@
 
     {{-- Body --}}
     <body class="font-sans leading-none {{ Config::get('app.env') }} {{ Env::get('APP_DUSK') }}">
+
+        {{-- Analytics --}}
+        @if (Config::get('services.google.analytics.key'))
+            @production
+
+                <!-- Google Tag Manager (noscript) -->
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ Config::get('services.google.analytics.key') }}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                <!-- End Google Tag Manager (noscript) -->
+
+            @endproduction
+        @endif
 
         {{-- Routes --}}
         @routes()
