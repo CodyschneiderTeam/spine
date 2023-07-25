8228c53eeee2f413faa707a6448291bab673f4ca
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2023-07-25 10:51:03 +0100

    added meta pixel

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -39,19 +39,36 @@
         <script src="{{ Config::get('theme.icons') }}" crossorigin="anonymous"></script>
 
         {{-- Analytics --}}
-        @if (Config::get('services.google.analytics.key'))
-            @production
+        @production
 
-                <!-- Google Tag Manager -->
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','{{ Config::get('services.google.analytics.key') }}');</script>
-                <!-- End Google Tag Manager -->
+            {{-- Google Tag Manager --}}
+            @if (Config::get('services.google.analytics.key'))
+                <script>
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','{{ Config::get('services.google.analytics.key') }}');
+                </script>
+            @endif
 
-            @endproduction
-        @endif
+            {{-- Meta Pixel --}}
+            @if (Config::get('services.meta.analytics.key'))
+                <script>
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '{{ Config::get('services.meta.analytics.key') }}');
+                    fbq('track', 'PageView');
+                </script>
+            @endif
+
+        @endproduction
 
         {{-- Slot --}}
         @includeIf('views.head')
@@ -65,16 +82,30 @@
     <body class="font-sans leading-none {{ Config::get('app.env') }} {{ Env::get('APP_DUSK') }}">
 
         {{-- Analytics --}}
-        @if (Config::get('services.google.analytics.key'))
-            @production
+        @production
 
-                <!-- Google Tag Manager (noscript) -->
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ Config::get('services.google.analytics.key') }}"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                <!-- End Google Tag Manager (noscript) -->
+            {{-- Google Tag Manager --}}
+            @if (Config::get('services.google.analytics.key'))
+                <noscript>
+                    <iframe width="0"
+                            height="0"
+                            style="display:none;visibility:hidden"
+                            src="https://www.googletagmanager.com/ns.html?id={{ Config::get('services.google.analytics.key') }}">
+                    </iframe>
+                </noscript>
+            @endif
 
-            @endproduction
-        @endif
+            {{-- Meta Pixel --}}
+            @if (Config::get('services.meta.analytics.key'))
+                <noscript>
+                    <img width="1"
+                         height="1"
+                         style="display:none"
+                         src="https://www.facebook.com/tr?id={{ Config::get('services.meta.analytics.key') }}&ev=PageView&noscript=1" />
+                </noscript>
+            @endif
+
+        @endproduction
 
         {{-- Routes --}}
         @routes()
