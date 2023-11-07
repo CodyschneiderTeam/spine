647e7dc6a65c8c1e6e037235819fab7e45d4081f
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2023-11-07 13:55:40 +0000

    WIP

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -2,6 +2,7 @@
 
 namespace System\Http;
 
+use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Config;
@@ -16,6 +17,19 @@ class Client
     public static function make() : PendingRequest
     {
         return Http::acceptJson()
+            ->timeout(App::isProduction() ? 40 : 0)
+            ->withUserAgent(Config::get('app.agent'))
+            ->connectTimeout(App::isProduction() ? 20 : 0)
+            ->retry(App::isProduction() ? 10 : 0, 5000, null, false);
+    }
+
+    /**
+     * Create an instance of the HTTP client using the given pool.
+     *
+     */
+    public static function makeUsingPool(Pool &$pool) : PendingRequest
+    {
+        return $pool->acceptJson()
             ->timeout(App::isProduction() ? 40 : 0)
             ->withUserAgent(Config::get('app.agent'))
             ->connectTimeout(App::isProduction() ? 20 : 0)
